449673b96ab8fd8bc14745cb0fdd2b81a31e853f
Vel <korzhavin.vel@yandex.ru>
2023-06-23 14:39:44 +0700

    homeWork3Task1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,66 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        task1();
+      /*  task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+
+       */
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+       //Объявите переменные типа int, byte, short, long, float, double.
+       // Выведите в консоль значение каждой переменной в формате «Значение переменной … с типом … равно …».
+        byte a = 2;
+            System.out.println("Значение переменной a с типом byte равно " + a);
+        short b = 200;
+            System.out.println("Значение переменной b с типом short равно " + b);
+        int g = 40000;
+            System.out.println("Значение переменной g с типом int равно " + g);
+        long d = 7777L;
+            System.out.println("Значение переменной d с типом long равно " + d);
+        float e = 3.5f;
+            System.out.println("Значение переменной e с типом float равно " + e);
+        double f = 1.7d;
+            System.out.println("Значение переменной f с типом double равно " + f);
 
     }
+    /*
+    public static void task2 () {
+        System.out.println("Задача 2");
+        ... // Пишем код для задачи 2
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        ... // Пишем код для задачи 3
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+        ... // Пишем код для задачи 4
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+        ... // Пишем код для задачи 5
+    }
+
+    public static void task6 () {
+        System.out.println("Задача 6");
+        ... // Пишем код для задачи 6
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+        ... // Пишем код для задачи 7
+    }
+
+    public static void task8 () {
+        System.out.println("Задача 8");
+        ... // Пишем код для задачи 8
+    }
+    */
 }
